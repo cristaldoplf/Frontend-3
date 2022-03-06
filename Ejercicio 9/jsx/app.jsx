3ccb0f764d225538ebd37ad2f1a7e851a6297e69
cristaldoplf <cristaldoplf@gmail.com>
2022-03-06 03:09:57 -0300

    cambio en el ejercicio 9

--- Ejercicio 9/jsx/app.jsx
+++ Ejercicio 9/jsx/app.jsx
@@ -2,7 +2,8 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 const listItems = numbers.map(number =>
     <React.Fragment>
-        <p><span>N°</span>{number}</p>
+        <span>N°</span>
+        <p>{number}</p>
     </React.Fragment>
 )
 
