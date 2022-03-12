3614f2103f253a20744e4e5f42149a961110cc18
cristaldoplf <cristaldoplf@gmail.com>
2022-03-12 02:06:37 -0300

    ejercicio 1 de complementario terminado

--- Complementario/proyecto-1/src/App.js
+++ Complementario/proyecto-1/src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
-  return (
+  let nombre = "Pablo";
+  let auth = true;
+  let estaciones = ["Primavera","Verano","Otoño","Invierno"]
+  return (  
+   <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre"/>
+        <h1>{nombre}</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {auth ? "El usuario estalogueado":"El usuario No esta loguead"}
         </p>
+        <p>{2+1}</p>
+        <p>
+          Editar <code>src/App.js</code> and save to reload.
+        </p>
+        <ul>
+          {estaciones.map((ele,i) => <li key={i}>{ele}</li>)}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +34,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
