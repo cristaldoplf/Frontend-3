ae8e036795f44c79aa2fbf063e7d8920642370cf
cristaldoplf <cristaldoplf@gmail.com>
2022-03-12 15:27:55 -0300

    tengo que borrar cosas en el proyecto para hacer una ejercitacion, dios bendiga los backups

---
+++ CTD/Ejercicio 13/ej13/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Paragraph from './components/Paragraph';
+import UlGenerator from './components/UlGenerator';
+
+const invitedList = [
+  {
+      name: "Nicolás",
+      task: "Traerá papas fritas",
+  }, 
+  {
+      name: "Iván",
+      task: "traerá pizzas",      
+  },
+  {
+      name: "Carolina",
+      task: "traerá bebidas",      
+  }
+]
+
+function App() {
+  return (
+    <div style={{borderStyle:"solid", width:"450px", marginLeft:"calc(50% - 225px)", marginTop:"100px"}} className="App">
+      <Paragraph weight="bold" text="Invitados:" />
+      <UlGenerator array={invitedList} type="invitados"/>
+      <Paragraph weight="bold" text="Tareas:" />
+      <UlGenerator array={invitedList} type="tareas"/>
+
+      
+      
+      
+    </div>
+  );
+}
+
+export default App;
