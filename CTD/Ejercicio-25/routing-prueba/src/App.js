ece59958a346e20fa20ebad7ae45784956e63d15
cristaldoplf <cristaldoplf@gmail.com>
2022-04-07 09:07:20 -0300

    agrego ejercicio 25 y 26 - router

--- CTD/Ejercicio-25/routing-prueba/src/App.js
+++ CTD/Ejercicio-25/routing-prueba/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 function Main() {
   return <h2>Main</h2>
@@ -13,10 +14,12 @@ function Home() {
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route exact path='/' element={<Main/>}/>
-        <Route path='/home'element={<Home/>}/>
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Main/>}/>
+          <Route path='/home'element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
